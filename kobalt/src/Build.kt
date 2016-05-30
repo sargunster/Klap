bb759f7244ad4fe4f25094ee021b10957ea43a11
Sargun Vohra <sargunster@gmail.com>
2016-05-30 18:22:07 -0400

    1.0.4

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -4,7 +4,7 @@ import com.beust.kobalt.plugin.publish.bintray
 
 object Versions {
     val kotlin = "1.0.2"
-    val cakeparse = "1.0.7"
+    val cakeparse = "1.0.8"
     val testng = "6.9.9"
 }
 
@@ -14,12 +14,12 @@ val p = project {
     name = "klap"
     group = "me.sargunvohra.lib"
     artifactId = name
-    version = "1.0.3"
+    version = "1.0.4"
 
     dependencies {
     	compile("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
         compile("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}")
-        compile("me.sargunvohra.lib:CakeParse:${Versions.cakeparse}")
+        compile("me.sargunvohra.lib:cakeparse:${Versions.cakeparse}")
     }
 
     dependenciesTest {
